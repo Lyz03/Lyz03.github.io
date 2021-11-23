cd3b58bdf9ee151740ada2268cc559977132673f
Lyz03 <lizoe@netcourrier.com>
2021-11-23 10:07:10 +0100

    eval morpion v2

--- Eval_realisation_du_jeu_morpion/appRDJM.js
+++ Eval_realisation_du_jeu_morpion/appRDJM.js
@@ -1,5 +1,5 @@
 // disable the context menu
-document.getElementById('play_set').addEventListener('contextmenu', function (event){
+document.getElementById('play_set').addEventListener('contextmenu', function (event) {
     event.preventDefault();
 });
 
@@ -17,113 +17,112 @@ isTouch = !!("ontouchstart" in window || navigator.msMaxTouchPoints);
 for (let i = 0; i < box.length; i++) {
     box[i].addEventListener('mouseup', function (e) {
 
-        // draw X
-        function left() {
-            box[i].innerHTML = 'X';
-        }
-
-        //draw O
-        function right() {
-            box[i].innerHTML = 'O';
-        }
-
         // right or left click, player turn & 1 or 2 players
-        function chooseRgtLft() {
-            if (enable === false) {
-                if (isTouch) {
-                    if (a % 2 === 1 && box[i].innerHTML !== 'X' && box[i].innerHTML !== 'O') {
-                        left();
+        if (enable === false) {
+            // touch device ?
+            if (isTouch) {
+                if (a % 2 === 1 && box[i].innerHTML !== 'X' && box[i].innerHTML !== 'O') {
+                    left(i);
+                    a++;
+                } else {
+                    if (choice.selectedIndex === 1) {
+                        pc();
+                        playerTurn.innerText = "Tour du joueur X";
                         a++;
-                    } else {
+                    }
+                    if (a % 2 === 0 && box[i].innerHTML !== 'X' && box[i].innerHTML !== 'O') {
+                        right(i);
+                        a++;
+                    }
+                }
+            } else {
+                // right or left click ?
+                switch (e.button) {
+                    case 0:
+                        if (choice.selectedIndex === 1) {
+                            playerTurn.innerText = "Tour du joueur O (clic droit pour faire jouer l'ordi)";
+                        } else {
+                            playerTurn.innerText = "Tour du joueur O";
+                        }
+                        if (a % 2 === 1 && box[i].innerHTML !== 'X' && box[i].innerHTML !== 'O') {
+                            left(i);
+                            a++;
+                        }
+                        break;
+                    case 2:
                         if (choice.selectedIndex === 1) {
                             pc();
                             playerTurn.innerText = "Tour du joueur X";
                             a++;
-                        }
-                        if (a % 2 === 0 && box[i].innerHTML !== 'X' && box[i].innerHTML !== 'O'){
-                            right();
+                        } else if (a % 2 === 0 && box[i].innerHTML !== 'X' && box[i].innerHTML !== 'O') {
+                            right(i);
+                            playerTurn.innerText = "Tour du joueur X";
                             a++;
                         }
-                    }
-                }
-                else {
-                    switch (e.button){
-                        case 0:
-                            if (choice.selectedIndex === 1) {
-                                playerTurn.innerText = "Tour du joueur O (clic droit pour faire jouer l'ordi)";
-                            } else {
-                                playerTurn.innerText = "Tour du joueur O";
-                            }
-                            if (a % 2 === 1 && box[i].innerHTML !== 'X' && box[i].innerHTML !== 'O') {
-                                left();
-                                a++;
-                            }
-                            break;
-                        case 2:
-                            if (choice.selectedIndex === 1) {
-                                pc();
-                                playerTurn.innerText = "Tour du joueur X";
-                                a++;
-                            } else if (a % 2 === 0 && box[i].innerHTML !== 'X' && box[i].innerHTML !== 'O') {
-                                right();
-                                playerTurn.innerText = "Tour du joueur X";
-                                a++;
-                            }
-                            break;
-                    }
+                        break;
                 }
             }
+
         }
 
-        chooseRgtLft();
         winCondition();
     });
+}
+
+// draw X
+function left(index) {
+    box[index].innerHTML = 'X';
+}
+
+//draw O
+function right(index) {
+    box[index].innerHTML = 'O';
 }
 
 // test if someone won
 function winCondition() {
     //line 1
-    if (box[0].innerHTML === box[1].innerHTML && box[1].innerHTML=== box[2].innerHTML){
+    if (box[0].innerHTML === box[1].innerHTML && box[1].innerHTML === box[2].innerHTML) {
         b = box[0].innerHTML;
         win()
     }
     // line 2
-    else if (box[3].innerHTML === box[4].innerHTML && box[4].innerHTML=== box[5].innerHTML) {
+    else if (box[3].innerHTML === box[4].innerHTML && box[4].innerHTML === box[5].innerHTML) {
         b = box[3].innerHTML;
         win();
     }
     // line 3
-    else if (box[6].innerHTML === box[7].innerHTML && box[7].innerHTML=== box[8].innerHTML) {
+    else if (box[6].innerHTML === box[7].innerHTML && box[7].innerHTML === box[8].innerHTML) {
         b = box[6].innerHTML;
         win();
     }
 
     // column 1
-    else if (box[0].innerHTML === box[3].innerHTML && box[3].innerHTML=== box[6].innerHTML) {
+    else if (box[0].innerHTML === box[3].innerHTML && box[3].innerHTML === box[6].innerHTML) {
         b = box[0].innerHTML;
         win();
     }
 
     //column 2
-    else if (box[1].innerHTML === box[4].innerHTML && box[4].innerHTML=== box[7].innerHTML) {
+    else if (box[1].innerHTML === box[4].innerHTML && box[4].innerHTML === box[7].innerHTML) {
         b = box[1].innerHTML;
         win();
     }
 
     // column 3
-    else if (box[2].innerHTML === box[5].innerHTML && box[5].innerHTML=== box[8].innerHTML) {
+    else if (box[2].innerHTML === box[5].innerHTML && box[5].innerHTML === box[8].innerHTML) {
         b = box[2].innerHTML;
         win();
     }
 
     // diagonal top left, bottom right
-    else if (box[0].innerHTML === box[4].innerHTML && box[4].innerHTML=== box[8].innerHTML) {
+    else if (box[0].innerHTML === box[4].innerHTML && box[4].innerHTML === box[8].innerHTML) {
         b = box[0].innerHTML;
         win();
     }
 
     // diagonal top right, bottom left
-    else if (box[2].innerHTML === box[4].innerHTML && box[4].innerHTML=== box[6].innerHTML) {
+    else if (box[2].innerHTML === box[4].innerHTML && box[4].innerHTML === box[6].innerHTML) {
         b = box[2].innerHTML;
         win();
     }
@@ -131,7 +130,7 @@ function winCondition() {
     // both players loose
     else {
         let c = 0;
-        for (let i = 0; i <box.length; i++){
+        for (let i = 0; i < box.length; i++) {
             if (box[i].innerHTML === 'X' || box[i].innerHTML === 'O') {
                 c++;
             }
@@ -145,7 +144,10 @@ function winCondition() {
     }
 }
 
-// What to do if someone win
+/* What to do if someone win :
+* add a sentence saying which player won
+* display the reset game button
+ */
 function win() {
     if (b === 'X') {
         playerTurn.innerText = 'Joueur X à gagné !'
@@ -171,17 +173,18 @@ function resetGame() {
     enable = false;
     playerTurn.style.fontSize = '1rem';
     playerTurn.innerText = 'Vous pouvez commencer, le joueur X avec le clic gauche et le joueur O avec le clic droit'
-        +'\n'+ 'Le joueur X commence';
+        + '\n' + 'Le joueur X commence';
     resetButton.style.display = 'none';
 }
 
-// player vs computer
-function pc () {
-    let boxToPlay = Math.floor(Math.random()*8)
+/* player vs computer
+* choose a random box and set a "O" in it
+ */
+function pc() {
+    let boxToPlay = Math.floor(Math.random() * 8)
     if (box[boxToPlay].innerHTML === "") {
-        box[boxToPlay].innerHTML = "0"
-    }
-    else if (box[boxToPlay].innerHTML !== "") {
+        box[boxToPlay].innerHTML = "O"
+    } else if (box[boxToPlay].innerHTML !== "") {
         pc();
     }
 }
